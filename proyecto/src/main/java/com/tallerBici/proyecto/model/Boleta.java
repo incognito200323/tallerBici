e3c2c49bc5963c5c1efacfddcf9b0d1327dfebbc
erick <erick.ponce4555@gmai.com>
2026-05-04 22:25:44 -0400

    Archivo nuevo

--- proyecto/src/main/java/com/tallerBici/proyecto/model/Boleta.java
+++ proyecto/src/main/java/com/tallerBici/proyecto/model/Boleta.java
@@ -1,5 +1,28 @@
 package com.tallerBici.proyecto.model;
 
+import java.util.List;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+
 public class Boleta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+
+
+
+  @ManyToMany
+  @JoinTable(name = "boletas_empleados",
+      joinColumns = @JoinColumn(name = "boleta_id"),
+      inverseJoinColumns = @JoinColumn(name = "empleado_id")
+    )
+    private List<Empleado> empleados;
 
 }
--- proyecto/src/main/java/com/tallerBici/proyecto/model/Empleado.java
+++ proyecto/src/main/java/com/tallerBici/proyecto/model/Empleado.java
@@ -1,16 +1,21 @@
 package com.tallerBici.proyecto.model;
 
+import java.util.List;
+
 import org.springframework.data.annotation.Id;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
@@ -22,8 +27,13 @@ public class Empleado {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     @NotBlank(message = "El campo nombres no puede estar vacío")
-    @Column(nullable = false, unique = true)
+    @Size(min = 3, max = 100, message = "El nombre debe tener entre 3 y 100 caracteres")
+    @Column(nullable = false, length = 100)
     private String nombres;
     @NotBlank(message = "El campo apellidos no puede estar vacío")
     private String apellidos;
+
+    @ManyToMany(mappedBy = "empleados")
+    @ToString.Exclude
+    private List<Boleta> boletas;   
 }
